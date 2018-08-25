9bb16468271f4769ec8f7b3a1e206261290ae52f
Wolverine <wolverine.karbon14@gmail.com>
2018-08-25 15:55:56 -0300

    fix lint helpers

--- Helpers/latestTime.js
+++ Helpers/latestTime.js
@@ -1,11 +1,11 @@
-const { getBlock } = require('./web3');
+const { getBlock } = require('./web3')
 
 // Returns the time of the last mined block in seconds
 const latestTime = async () => {
-  const block = await getBlock('latest');
-  return block.timestamp;
+  const block = await getBlock('latest')
+  return block.timestamp
 }
 
 module.exports = {
   latestTime,
-};
+}
